f77bad60c40fc799da9c8db6a86e549bf49273e2
BaxanAcharya <baxanacharya@gmail.com>
2019-11-07 13:01:13 +0545

    "Area of circle and adding two numbers and fragments"

--- app/src/main/java/com/biplav/fragments/MainActivity.java
+++ app/src/main/java/com/biplav/fragments/MainActivity.java
@@ -11,35 +11,37 @@ import android.widget.Button;
 import com.biplav.fragments.fragments.FirstFragment;
 import com.biplav.fragments.fragments.SecondFragment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btn;
-    String display="first";
+    String display = "first";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=findViewById(R.id.btn1);
+        btn = findViewById(R.id.btn1);
         btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
-        if(display.equals("first")){
-            FirstFragment firstFragment=new FirstFragment();
-            fragmentTransaction.replace(R.id.lineraLayout,firstFragment);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (display.equals("first")) {
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.replace(R.id.lineraLayout, firstFragment);
+            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
             btn.setText("Load Second Fragment");
-            display="second";
-        }
-        else if(display.equals("second")){
-            SecondFragment secondFragment=new SecondFragment();
-            fragmentTransaction.replace(R.id.lineraLayout,secondFragment);
+            display = "second";
+        } else if (display.equals("second")) {
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.replace(R.id.lineraLayout, secondFragment);
+            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
             btn.setText("Load First Fragment");
-            display="first";
+            display = "first";
         }
     }
 }
